cf6187b79327c433a0c25a24a55887a8f6a35444
Bjarte Johansen <bjo013@uib.no>
2013-02-07 19:55:20 +0100

    add OpenFileInBuffer functionality

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"os"
 	"github.com/mattn/go-gtk/gtk"	
 	gsv "github.com/mattn/go-gtk/gtksourceview"	
 )
 
+func OpenFileInBuffer(tb *gsv.SourceBuffer,f string) (err error) {
+	var (
+		part []byte
+		prefix bool
+		start gtk.TextIter
+	)
+
+	file, err := os.Open(f)
+	if err != nil {
+		return
+	}	
+	
+	tb.GetStartIter(&start)
+
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 1024))
+	
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			return err
+		}
+		buffer.Write(part)
+		if !prefix {
+			tb.Insert(&start, buffer.String()+"\n")
+			buffer.Reset()
+		}
+	}
+	file.Close()
+	return nil
+}
 func main() {
 	gtk.Init(&os.Args)
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
