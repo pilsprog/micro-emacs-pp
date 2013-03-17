d80cc3ff704556b0282b8d2def6c4e846f5cbd26
haskellelephant <eivind.jahren@gmail.com>
2013-03-17 01:30:59 +0100

    Minor cleanup in microemacspp.go

--- microemacspp.go
+++ microemacspp.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"os"
-	"unsafe"
-	"github.com/mattn/go-gtk/gtk"
-	"github.com/mattn/go-gtk/gdk"
-	"github.com/mattn/go-gtk/glib"
-	gsv "github.com/mattn/go-gtk/gtksourceview"
+  "os"
+  "unsafe"
+  "github.com/mattn/go-gtk/gtk"
+  "github.com/mattn/go-gtk/gdk"
+  "github.com/mattn/go-gtk/glib"
+  gsv "github.com/mattn/go-gtk/gtksourceview"
   "micro-emacs-pp/Editor"
   "micro-emacs-pp/KeyHandler"
-  "fmt"
 )
 
 var textbuffer * gtk.TextBuffer
@@ -21,21 +20,21 @@ var keyh KeyHandler.KeyHandler = KeyHandler.MakeRoot()
 
 func main() {
 
-	gtk.Init(&os.Args)
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
-	window.SetTitle("µemacs/pp")
-	window.Connect("destroy", gtk.MainQuit)
+  gtk.Init(&os.Args)
+  window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+  window.SetTitle("µemacs/pp")
+  window.Connect("destroy", gtk.MainQuit)
   window.Connect("key-press-event",handleKeyPress);
 
-	swin := gtk.NewScrolledWindow(nil, nil)
-	sourcebuffer := gsv.NewSourceBufferWithLanguage(gsv.SourceLanguageManagerGetDefault().GetLanguage("cpp"))
-	sourceview = gsv.NewSourceViewWithBuffer(sourcebuffer)
+  swin := gtk.NewScrolledWindow(nil, nil)
+  sourcebuffer := gsv.NewSourceBufferWithLanguage(gsv.SourceLanguageManagerGetDefault().GetLanguage("cpp"))
+  sourceview = gsv.NewSourceViewWithBuffer(sourcebuffer)
 
-	var start gtk.TextIter
-	sourcebuffer.GetStartIter(&start)
-	sourcebuffer.Insert(&start, `writing stuff, awww yea!`)
+  var start gtk.TextIter
+  sourcebuffer.GetStartIter(&start)
+  sourcebuffer.Insert(&start, `writing stuff, awww yea!`)
 
-	textview = gtk.NewTextView()
+  textview = gtk.NewTextView()
   textbuffer = textview.GetBuffer()
 
   var bufiter gtk.TextIter
@@ -46,28 +45,25 @@ func main() {
   comWrapper := Editor.GtkTextBufferReadWriter{&textview.Container.Widget,comiter,textbuffer}
   microemacs = Editor.Editor{"",&bufWrapper,&comWrapper}
 
-	vbox := gtk.NewVBox(false,0)
-	vbox.PackStart(swin, true, true,0)
-	vbox.PackEnd(textview,false, true,0)
+  vbox := gtk.NewVBox(false,0)
+  vbox.PackStart(swin, true, true,0)
+  vbox.PackEnd(textview,false, true,0)
 
-	swin.Add(sourceview)
+  swin.Add(sourceview)
 
-	window.Add(vbox)
-	window.SetSizeRequest(300, 300)
-	window.ShowAll()
+  window.Add(vbox)
+  window.SetSizeRequest(300, 300)
+  window.ShowAll()
 
-	gtk.Main()
+  gtk.Main()
 }
 
 func handleKeyPress(ctx *glib.CallbackContext){
   arg := ctx.Args(0)
   kev := *(**gdk.EventKey)(unsafe.Pointer(&arg))
 
-  fmt.Println("---------------------\n\n")
-
   kpe := KeyHandler.KeyPressEvent{int(kev.Keyval),0}
   if (gdk.ModifierType(kev.State) & gdk.CONTROL_MASK) != 0{
-    fmt.Println("Added Control Mask")
     kpe.Modifier = gdk.CONTROL_MASK
   }
 
